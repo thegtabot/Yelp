95799d5f6e20c5d57ba41b01e374cc4467dbfd2d
user141707 <user141707@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-09-26 16:19:02 -0400

    Initial Commit

---
+++ Parse Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parse Chat
+//
+//  Created by user141707 on 9/26/18.
+//  Copyright © 2018 user141707. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
